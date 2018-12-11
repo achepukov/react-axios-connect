0258aacac1b587c6e3fb636c46ec9a82cef24e49
Alex Chepukov <a.chepukov@gmail.com>
2018-12-11 09:21:04 +0300

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname),
+    libraryTarget: "umd",
+    filename: 'main.js',
+  },
   module: {
     rules: [
       {
